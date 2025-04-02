8040db39f1d504afeef2754200701377fd1d93bb
gitiago4 <ils4@cin.ufpe.br>
2025-04-02 13:02:02 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame as pg
-
+#Criação da classe do player, com seus respectivos métodos e atributos.
 class Player(pg.sprite.Sprite):
+    #Inicializando a classe com atributos de posição, escala da imagem, velocidade e etc...
     def __init__(self, x, y, escala, velocidade):
         pg.sprite.Sprite.__init__(self)
         img = pg.image.load(f'imagem/0.png')
@@ -10,7 +11,7 @@ class Player(pg.sprite.Sprite):
         self.imagem = pg.transform.scale(img, (int(img.get_width()*escala), int(img.get_height()*escala)))
         self.rect = self.imagem.get_rect()
         self.rect.center = (x,y)
-    
+    #Método responsável pelo movimento do personagem.
     def move(self, andaresquerda, andardireita, largura):
         dx = 0
         if(self.rect.x <= 0):
@@ -26,7 +27,7 @@ class Player(pg.sprite.Sprite):
             self.virar = False
             self.direcao = 1
         self.rect.x += dx
-
+    #Método que faz com que o personagem seja impresso na tela do jogo.
     def draw(self, tela):
        tela.blit(pg.transform.flip(self.imagem, self.virar, False), self.rect)
         
