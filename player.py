769eaa0eeacf64f25fb7031a8c4e52f6144261d1
gabyalvesfv <gafv@cin.ufpe.br>
2025-04-07 16:36:36 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,9 +1,8 @@
 import pygame as pg
 
-#Criação da classe do player, com seus respectivos métodos e atributos.
 class Player(pg.sprite.Sprite):
     
-    #Inicializando a classe com atributos de posição, escala da imagem, velocidade e etc...
+    # Inicialização da classe com atributos de posição, escala da imagem, velocidade
     def __init__(self, x, y, escala, velocidade):
         pg.sprite.Sprite.__init__(self)
         self.vidas = 3
@@ -16,7 +15,7 @@ class Player(pg.sprite.Sprite):
         self.update_time = pg.time.get_ticks()
         listatemp = []
         
-        #Armazena todos os frames da animação em que o personagem esta parado no indice 0 da lista de animação
+        #Armazenamento de todos os frames da animação em que o personagem esta parado no indice 0 da lista de animação
         for i in range(2):
             img = pg.image.load(f'imagens/spritesheet/Idle/{i}.png')
             img = pg.transform.scale(img, (int(img.get_width()*escala), int(img.get_height()*escala)))
@@ -24,7 +23,7 @@ class Player(pg.sprite.Sprite):
         self.lista_animacao.append(listatemp)
         listatemp=[]
 
-        #Armazena todos os frames da animação em que o personagem esta correndo no indice 1 da lista de animação
+        # Armazenamento de todos os frames da animação em que o personagem esta correndo no indice 1 da lista de animação
         for i in range(5):
             img = pg.image.load(f'imagens/spritesheet/Run/{i}.png')
             img = pg.transform.scale(img, (int(img.get_width()*escala), int(img.get_height()*escala)))
@@ -35,7 +34,7 @@ class Player(pg.sprite.Sprite):
         self.rect = self.imagem.get_rect()
         self.rect.center = (x,y)
         
-    #Método responsável pelo movimento do personagem.
+    # Movimento do personagem
     def move(self, andaresquerda, andardireita, largura):
         dx = 0
         if(self.rect.x <= 0):
@@ -52,33 +51,29 @@ class Player(pg.sprite.Sprite):
             self.direcao = 1
         self.rect.x += dx
         
-    #Método responsável por percorrer os quadros da animaçaõ do personagem
+    # Percorre os quadros da animação do personagem
     def update_animation(self):
-        #Tempo entre um quadro e outro da animação (neste caso, 100 milisegundos)
+        # Intervalo de tempo entre os quadros de animação (100 milisegundos)
         cooldown = 100
         self.imagem = self.lista_animacao[self.acao][self.indiceframe]
         if pg.time.get_ticks()-self.update_time > cooldown:
             self.update_time = pg.time.get_ticks()
             self.indiceframe += 1
-        #Caso a lista chegue ao fim, faz com que retorne ao começo, iniciando assim o loop da animação
+        # Caso a lista chegue ao fim, retorna ao começo e inicia o loop da animação
         if self.indiceframe >= len(self.lista_animacao[self.acao]):
             self.indiceframe = 0
             
-    #Método responsavel por passar de uma açaõ para a outra (personagem parado -> personagem correndo e vice-versa)
+    # Passagem de uma ação para a outra (personagem parado -> personagem correndo e vice-versa)
     def update_action(self, nova_acao):
         if nova_acao != self.acao:
             self.acao = nova_acao
             self.indiceframe = 0
             self.update_time = pg.time.get_ticks()
             
-    #Método que faz com que o personagem seja impresso na tela do jogo.
+    # Impressão do personagem 
     def draw(self, tela):
        tela.blit(pg.transform.flip(self.imagem, self.virar, False), self.rect)
         
-    #Metódo responsavel por diminuir a vida do personagem.
+    # Perda de vida 
     def lose_lives(self):
         self.vidas-=1
-        
-
-    
-
